39bc815ccbc84bfdc6f7e61090a5fb8d11fdb664
Adam Albarghouthi <adambargh@gmail.com>
2024-09-26 11:06:11 -0400

    add convertToCodepoints, orderShadda funcs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { diacritics, diacriticsCodePoints } from "./diacritics";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -12,3 +14,32 @@ export const difficultyItems: { value: DifficultyType; label: string }[] = [
   { value: "medium", label: "Medium" },
   { value: "hard", label: "Hard" },
 ];
+
+export const convertToCodepoints = (str: string) => {
+  return [...str].map((c) => c.codePointAt(0) as number);
+};
+
+const SHADDA_CODEPOINT = 1617;
+
+// ensure any shadda is before its corresponding haraka
+export const orderShadda = (arr: number[]) => {
+  const result = [];
+  let pos = 0;
+
+  while (pos < arr.length) {
+    if (
+      arr[pos] !== SHADDA_CODEPOINT &&
+      diacriticsCodePoints.includes(arr[pos]) &&
+      arr[pos + 1] === SHADDA_CODEPOINT
+    ) {
+      result.push(arr[pos + 1]);
+      result.push(arr[pos]);
+      pos += 2;
+    } else {
+      result.push(arr[pos]);
+      pos += 1;
+    }
+  }
+
+  return result;
+};
