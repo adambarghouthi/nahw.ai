c0632aa744a187b18afbbaade1e163c739723b6d
Adam Albarghouthi <adambargh@gmail.com>
2024-09-26 11:52:47 -0400

    modify json schema

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -6,9 +6,8 @@ import { DifficultyType } from "./utils";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-type AIJsonSchema = {
-  plain: string;
-  diacritic: string;
+type SentenceObjectType = {
+  arabic: string;
   translation: string;
   word_mapping: {
     arabic: string;
@@ -78,8 +77,7 @@ const generateSentence = async (
           
           I want you to return only JSON in the following schema:
             {
-              "plain": "string", 
-              "diacritic": "string", 
+              "arabic": "string", 
               "translation": "string", 
               "word_mapping": [
                 {
@@ -91,14 +89,15 @@ const generateSentence = async (
             }
             
           Here is the definition for each key in the above JSON:
-            - plain: An Arabic sentence with absolutely NO diacritics on any of its letters.
-            - diacritic: An Arabic sentence that follows the rules of Arabic Nahw (grammar) with the correct diacritics applied. The sentence should adhere to the following structure:
+            - arabic: An Arabic sentence that follows the rules of Arabic Nahw (grammar) with the correct diacritics applied. The sentence should adhere to the following structure:
               - Include diacritics for proper subject-verb-object agreement and case endings.
               - Use appropriate i‘rāb (nominative, accusative, genitive) based on the sentence components.
               - Ensure verbs are conjugated correctly based on tense and subject agreement.
               - Avoid adding diacritics to the "ال" prefix.
+              - Include diacritics specific to word's phonetics.
             - translation: The translation of the plain sentence in ${trans}.
-            - word_mapping: An array mapping each Arabic word (without diacritics) to its translation in ${trans}, along with its index in the plain sentence.
+            - word_mapping: An array mapping each Arabic word (without diacritics) to its translation in ${trans}, along with its index indicating its position in the Arabic sentence
+              where starting index is 0.
 
           Now generate a random ${difficulty} Arabic sentence and return the corresponding JSON object.
         `.trim(),
@@ -106,5 +105,5 @@ const generateSentence = async (
     ],
   });
 
-export type { AIJsonSchema };
+export type { SentenceObjectType };
 export { generateSentence };
