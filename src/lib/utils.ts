7d7445d8924cacf8698089e45791183aa19f35ee
Adam Albarghouthi <adambargh@gmail.com>
2024-11-13 13:55:20 -0500

    minor

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { diacritics, diacriticsCodePoints } from "./diacritics";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+import { diacriticsCodePoints } from "./diacritics";
 
 export type DifficultyType = "easy" | "medium" | "hard";
+
+const SHADDA_CODEPOINT = 1617;
 
 export const difficultyItems: { value: DifficultyType; label: string }[] = [
   { value: "easy", label: "Easy" },
@@ -15,11 +13,54 @@ export const difficultyItems: { value: DifficultyType; label: string }[] = [
   { value: "hard", label: "Hard" },
 ];
 
+export const difficultyPrompt: Record<
+  DifficultyType,
+  (topic: string) => string
+> = {
+  easy: (topic: string) =>
+    `Generate a short (5-6 words) arabic sentence that you would find in a children's book on the topic of ${topic}`,
+  medium: (topic: string) =>
+    `Generate a short (7-8 words) arabic sentence that you would find in novels and history books on the topic of ${topic}`,
+  hard: (topic: string) =>
+    `Generate a short (9-11 words) arabic sentence that you would find in classical textbooks and writings like hadith on the topic of ${topic}`,
+};
+
+export const topics: string[] = [
+  "Productivity",
+  "Politics",
+  "Sports",
+  "Technology",
+  "Health",
+  "Finance",
+  "Religion",
+  "Education",
+  "Entertainment",
+  "Science",
+  "Travel",
+  "Lifestyle",
+  "Environment",
+  "Business",
+  "Food",
+  "Art",
+  "History",
+  "Literature",
+  "Music",
+  "Gaming",
+  "Fashion",
+];
+
+export function getRandomTopic(): string {
+  const randomIndex = Math.floor(Math.random() * topics.length);
+  return topics[randomIndex];
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const convertToCodepoints = (str: string) => {
   return [...str].map((c) => c.codePointAt(0) as number);
 };
-
-const SHADDA_CODEPOINT = 1617;
 
 // ensure any shadda is before its corresponding haraka
 export const orderShadda = (arr: number[]) => {
