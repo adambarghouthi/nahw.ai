3d5924dca973496469b40600ee51243fccb78b84
Adam Albarghouthi <adambargh@gmail.com>
2024-11-28 15:01:45 -0500

    add diacritic char under mutable char

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,3 +1,7 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+import { removeDiacritics } from "@/lib/shaping";
+
 import Tooltip from "./Tooltip";
 import Character from "./Character";
 
@@ -5,39 +9,78 @@ interface WordProps {
   index: number;
   translation: string;
   charGroups: string[];
+  diacriticCharGroups: string[];
+  showDiacritics: boolean;
+  selectedChar?: number[];
   range: {
     start: number;
     end: number;
   };
-  onCharSelect: (index: number) => void;
+  onCharSelect: (
+    wIdx: number,
+    cIdx: number,
+    addIdx: number,
+    coords: number[]
+  ) => void;
 }
 
 export default function Word({
   index,
   translation,
   charGroups,
+  diacriticCharGroups,
   range,
+  showDiacritics,
+  selectedChar,
   onCharSelect,
 }: WordProps) {
   return (
     <Tooltip content={translation}>
-      <p id="word" className="flex rounded-lg px-2 py-1.5 hover:bg-muted">
+      <div
+        id="word"
+        className="relative flex rounded-lg px-2 py-1.5 hover:bg-muted"
+      >
         {charGroups.map((c, cIdx) => {
           return (
-            <Character
-              key={cIdx + c}
-              char={c}
-              onSelect={() =>
-                c !== " "
-                  ? onCharSelect(
-                      cIdx + range.start + index /* to account for spaces */
-                    )
-                  : null
-              }
-            />
+            <div key={cIdx} className="relative">
+              <div className="relative z-10">
+                <Character
+                  key={cIdx + c}
+                  char={showDiacritics ? removeDiacritics(c) : c}
+                  selected={
+                    index === selectedChar?.[0] && cIdx === selectedChar?.[1]
+                  }
+                  onSelect={(coords) => {
+                    onCharSelect(
+                      index,
+                      cIdx,
+                      cIdx + range.start + index /* to account for spaces */,
+                      coords
+                    );
+                  }}
+                />
+              </div>
+
+              <AnimatePresence>
+                {showDiacritics && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.3, ease: "easeOut" }}
+                    exit={{ opacity: 0 }}
+                    className="absolute top-0 left-0 text-green-500"
+                  >
+                    <Character
+                      key={cIdx + c}
+                      char={diacriticCharGroups[cIdx]}
+                    />
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
           );
         })}
-      </p>
+      </div>
     </Tooltip>
   );
 }
