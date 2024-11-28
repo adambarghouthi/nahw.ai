781716163f94b32c6fb3388f710f5fb14d1960a8
Adam Albarghouthi <adambargh@gmail.com>
2024-11-28 15:00:55 -0500

    redesign

--- src/components/DiacriticsMenubar.tsx
+++ src/components/DiacriticsMenubar.tsx
@@ -1,15 +1,18 @@
 import { diacritics } from "@/lib/diacritics";
+import { Trash } from "lucide-react";
+import { motion } from "framer-motion";
 
-import { Toggle } from "./ui/toggle";
 import * as DiacriticIcons from "./icons";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
 
 interface DiacriticsMenubarProps {
-  selected: string | null;
+  coords: number[];
   onSelect: (diacritic: string) => void;
 }
 
 export default function DiacriticsMenubar({
-  selected,
+  coords,
   onSelect,
 }: DiacriticsMenubarProps) {
   const renderToggleIcon = (name: string) => {
@@ -34,17 +37,83 @@ export default function DiacriticsMenubar({
   };
 
   return (
-    <div className="flex flex-row items-center rounded-lg gap-x-1 border border-primary/15 p-1 shadow-lg">
-      {diacritics.map((d) => (
-        <Toggle
-          key={d.name}
-          className="size-10"
-          pressed={selected === d.name}
-          onPressedChange={() => onSelect(d.name)}
-        >
-          {renderToggleIcon(d.name)}
-        </Toggle>
-      ))}
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+      className="fixed flex flex-row items-center rounded-lg gap-x-1 bg-primary-foreground border border-primary/15 p-1 shadow-lg z-20 transition-[top,left] duration-300 ease-in-out"
+      style={{
+        left: coords[0] - 65,
+        top: coords[1] + 50,
+      }}
+    >
+      <div className="grid grid-cols-3 gap-1">
+        {diacritics
+          .slice(0, 3)
+          .reverse()
+          .map((d) => (
+            <Button
+              key={d.name}
+              className="size-9"
+              onClick={() => onSelect(d.name)}
+              variant="ghost"
+              size="icon"
+            >
+              {renderToggleIcon(d.name)}
+            </Button>
+          ))}
+        {diacritics
+          .slice(3, 6)
+          .reverse()
+          .map((d) => (
+            <Button
+              key={d.name}
+              className="size-9"
+              onClick={() => onSelect(d.name)}
+              variant="ghost"
+              size="icon"
+            >
+              {renderToggleIcon(d.name)}
+            </Button>
+          ))}
+        {[null, ...diacritics.slice(6, 8).reverse()].map((d) =>
+          d ? (
+            <Button
+              key={d.name}
+              className="size-9"
+              onClick={() => onSelect(d.name)}
+              variant="ghost"
+              size="icon"
+            >
+              {renderToggleIcon(d.name)}
+            </Button>
+          ) : (
+            <Button
+              key="trash"
+              className="bg-red-800 bg-opacity-20 size-9 hover:bg-opacity-30 hover:bg-red-800"
+              onClick={() => onSelect("trash")}
+              variant="ghost"
+              size="icon"
+            >
+              <Trash className="h-4 w-4 text-red-700" />
+            </Button>
+          )
+        )}
+        <div className="col-span-3">
+          <Separator />
+        </div>
+        <div className="col-span-3">
+          <Button
+            size="sm"
+            className="w-full"
+            onClick={() => onSelect("close")}
+            variant="ghost"
+          >
+            Close
+          </Button>
+        </div>
+      </div>
+    </motion.div>
   );
 }
