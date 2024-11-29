1a2c5875ef144bc75498fdc13cecef6e73a3a73a
Adam Albarghouthi <adambargh@gmail.com>
2024-11-29 13:25:20 -0500

    initial commit

---
+++ src/components/WordContextMenu.tsx
@@ -0,0 +1,31 @@
+import { wordContextActions } from "@/lib/utils";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "./ui/context-menu";
+
+interface WordContextMenuProps {
+  onClick: (action: string) => void;
+  children: React.ReactNode;
+}
+
+export default function WordContextMenu({
+  onClick,
+  children,
+}: WordContextMenuProps) {
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger>{children}</ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem onClick={() => onClick(wordContextActions.IRAAB)}>
+          ʾIʿrāb
+        </ContextMenuItem>
+        <ContextMenuItem onClick={() => onClick(wordContextActions.DEFINITION)}>
+          Definition
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
+  );
+}
